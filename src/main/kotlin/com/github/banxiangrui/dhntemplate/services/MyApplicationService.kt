0ab2ea6c00394c481d99d9557f4686e814b0eab6
GitHub Action <action@github.com>
2021-07-02 10:26:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/banxiangrui/dhntemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/banxiangrui/dhntemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.banxiangrui.dhntemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.banxiangrui.dhntemplate.MyBundle
 
 class MyApplicationService {
 
